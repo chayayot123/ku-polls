f73efbacc0bd991a4a7e713b0db1e85289a15979
chayayot123 <chayayot123@hotmail.com>
2021-09-03 19:54:25 +0700

    finish django tutorial part5

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -14,7 +14,8 @@ class Question(models.Model):
 
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
@@ -22,6 +23,6 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
-    
+
     def __str__(self):
         return self.choice_text
