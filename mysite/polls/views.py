f189a3a526d8638c176579b9566d0424365bcd4f
chayayot123 <chayayot123@hotmail.com>
2021-09-03 19:08:19 +0700

    forget to import genetic

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,6 +4,7 @@ from django.template import loader
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from .models import Choice, Question
+from django.views import generic
 
 
 class IndexView(generic.ListView):
