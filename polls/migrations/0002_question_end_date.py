48c605f82379e6ecb3ca6ce6b79ccbbc2adc018a
chayayot123 <chayayot123@hotmail.com>
2021-09-11 19:14:41 +0700

    Update the data base schema

---
+++ polls/migrations/0002_question_end_date.py
@@ -0,0 +1,18 @@
+from django.db import migrations, models
+import django.utils.timezone
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('polls', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='question',
+            name='end_date',
+            field=models.DateTimeField(default=django.utils.timezone.now, verbose_name='date ended'),
+            preserve_default=False,
+        ),
+    ]
