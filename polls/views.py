36d90b203931fbc9ae0b289906f8ab609c28a0c0
chayayot123 <chayayot123@hotmail.com>
2021-09-18 16:01:03 +0700

    Improve and fixed format

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, request
 from django.template import loader
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
