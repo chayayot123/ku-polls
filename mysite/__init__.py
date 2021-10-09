774a8fe74482647f9ecb24c2a3f9d3a81e586521
chayayot123 <chayayot123@hotmail.com>
2021-10-09 14:05:30 +0700

    Update format and documents by using flake8

---
+++ mysite/__init__.py
@@ -0,0 +1,1 @@
+"""Initail commit for flake8."""
--- polls/apps.py
+++ polls/apps.py
@@ -1,6 +1,10 @@
+"""Config for ku-polls app."""
+
 from django.apps import AppConfig
 
 
 class PollsConfig(AppConfig):
+    """Polls config."""
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'polls'
